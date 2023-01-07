f926dd6137b7e71f04506fa44eb159484392654a
김주열 <100749830+dufwn1234@users.noreply.github.com>
2023-01-07 14:50:40 +0900

    Add files via upload

---
+++ Bruteforcing/beakjoon-7568.py
@@ -0,0 +1,23 @@
+from collections import *
+n=int(input())
+a=[]
+for i in range(n):
+    a.append(list(map(int,input().split())))
+b=defaultdict(int)
+count=1
+for i in range(len(a)):
+    for j in range(len(a)):
+        if i==j:
+            continue
+        else:
+            if a[i][0]>=a[j][0] and a[i][1]>=a[j][1]:
+                continue
+            elif a[i][0]>=a[j][0] and a[i][1]<=a[j][1]:
+                continue
+            elif a[i][0]<=a[j][0] and a[i][1]>=a[j][1]:
+                continue
+            elif a[i][0]<a[j][0] and a[i][1]<a[j][1]:
+                count+=1
+    print(count,end=" ")
+    count=1
+
